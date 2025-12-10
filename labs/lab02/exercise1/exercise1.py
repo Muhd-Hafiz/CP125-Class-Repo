ad406d910ca612ea9825dd52dc27e3fb73757358
Muhd-Hafiz <hafizjaludin16@gmail.com>
2025-12-10 12:22:21 +0800

    push

--- labs/lab02/exercise1/exercise1.py
+++ labs/lab02/exercise1/exercise1.py
@@ -1,10 +1,13 @@
-# Lab 02 Exercise 1: Road Trip Budgeter
-# Write your code below:
-
 def is_budget_sufficient(one_way_km, km_per_liter, price_per_liter, budget):
-    # TODO: Implement this function
-    # Calculate round trip cost and checks if within budget
-    pass
+    round_trip = (one_way_km * 2) / km_per_liter * price_per_liter
+    if round_trip <= budget:
+        return True
+    else:
+        return False
 
-# Test your code here
-print("Testing Road Trip Budgeter...")
+
+
+    
+    
+    
+    
--- labs/lab02/exercise2/exercise2.py
+++ labs/lab02/exercise2/exercise2.py
@@ -1,12 +1,8 @@
-# Lab 02 Exercise 2: Camping Logistics
-# Write your code below:
-
 import math
 
 def calculate_event_cost(participants, tent_capacity, tent_price, meal_price):
-    # TODO: Implement this function
-    # Calculate total cost for tents and meals
-    pass
-
+    cost_tents = math.ceil(participants / tent_capacity) * tent_price
+    total_budget = cost_tents + (meal_price * participants)
+    return total_budget
 # Test your code here
 print("Testing Camping Logistics...")
