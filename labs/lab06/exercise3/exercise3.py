9cbd771bff21eb66f899b1caea046bc0449db590
Muhd-Hafiz <hafizjaludin16@gmail.com>
2026-01-28 12:47:26 +0800

    push

--- labs/lab06/exercise3/exercise3.py
+++ labs/lab06/exercise3/exercise3.py
@@ -1,2 +1,11 @@
 def audit_blocklists(list_a, list_b, list_c):
-    pass
+    A = set(list_a)
+    B = set(list_b)
+    C = set(list_c)
+
+    Universal = A & B & C
+    Redundant = (A & B) | (B & C) | (A & C)
+    Unique_A_Only = A - B - C
+
+    return Universal, Redundant, Unique_A_Only
+    
--- labs/lab06/exercise5/exercise5.py
+++ labs/lab06/exercise5/exercise5.py
@@ -1,2 +1,19 @@
 def audit_zero_trust(baseline_set, current_log_list):
-   pass
+   current_log = set()
+   for log in current_log_list:
+         current_log.add(log)
+   
+   authorized = set()
+   Alerts = set()
+   for entry in current_log:
+      if entry not in baseline_set:
+         authorized.add(entry)
+      else:
+         Alerts.add(entry)
+
+   inactive = set()
+   for entry in baseline_set:
+      if entry not in current_log:
+         inactive.add(entry)
+
+   return Alerts, authorized, inactive
