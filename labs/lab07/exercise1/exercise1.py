dde712721d1bf2e97842862734d07055c467cb3a
Muhd-Hafiz <hafizjaludin16@gmail.com>
2026-02-25 13:00:31 +0800

    push

--- labs/lab07/exercise1/exercise1.py
+++ labs/lab07/exercise1/exercise1.py
@@ -1,9 +1,18 @@
 def process_actions(catalog, actions):
     # TODO: Your code here
-    pass
+        for action, isbn in actions:
+            if action == "BORROW":
+                if isbn in catalog and catalog[isbn] > 0:
+                    catalog[isbn] -= 1
+                else:
+                    return "ERROR"
+            elif action == "RETURN":
+                if isbn in catalog:
+                    catalog[isbn] += 1
+                else:
+                    return "ERROR"
 
-
-
+        return catalog
 catalog = {
     "978-A": 2,
     "978-B": 0,
